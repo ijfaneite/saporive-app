d6b1ba2a1d0bb2a5f545dcfa09d0a388fd6d7d6b
Ivan Faneite <ijfaneite@gmail.com>
2026-01-30 09:30:34 +0000

    el formato del numero de pedido es "YYMMDD-000" año, mes y dia 2 digitos

--- src/lib/data-provider.tsx
+++ src/lib/data-provider.tsx
@@ -211,7 +211,7 @@ export const DataProvider = ({ children }: { children: ReactNode }) => {
             nextIdCounter++;
             
             const date = new Date();
-            const year = date.getFullYear();
+            const year = String(date.getFullYear()).slice(-2);
             const month = String(date.getMonth() + 1).padStart(2, '0');
             const day = String(date.getDate()).padStart(2, '0');
             const nextIdStr = String(nextIdCounter).padStart(3, '0');
